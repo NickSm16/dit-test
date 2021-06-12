322638dec533f13bde27b30a93692285ac6a5332
nick_sm <smekhov.nikolai@gmail.com>
2021-06-12 20:30:26 +0300

    add map (leaflet + osm)

---
+++ src/components/map.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="mapContainer"></div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+  name: "Map",
+  data() {
+    return {
+      center: [55.755826, 37.6173],
+    };
+  },
+  methods: {
+    setupLeafletMap: function () {
+      const mapDiv = L.map("mapContainer").setView(this.center, 11);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+        mapDiv
+      );
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
+};
+</script>
+
+<style scoped>
+#mapContainer {
+  width: 80vw;
+  height: 90vh;
+}
+</style>
