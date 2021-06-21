02d7c45aa5a9ab09b5b926716c9b1278ba0aafe7
nick_sm <smekhov.nikolai@gmail.com>
2021-06-21 15:15:29 +0300

    вынес метод генерации в компонент map

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="wrapper">
     <div class="mapContainer">
-      <Map :points="points" />
+      <Map 
+        ref="map"
+        :points="points"
+      />
     </div>
     <div class="sidebar">
       <div class="btn-container">
         <button
           class="btn"
-          @click="generate()"
+          @click="generate"
         >
           Сгенерировать
         </button>
@@ -21,13 +24,8 @@
 </template>
 
 <script>
-import * as turf from "@turf/turf";
-
 import Map from "./components/Map.vue";
 import UnitChart from "./components/Chart.vue";
-
-import moscow from "../public/moscow.json";
-import admin_units from "../public/admin_units.json";
 
 export default {
   name: "App",
@@ -41,40 +39,10 @@ export default {
     };
   },
   methods: {
-    generate: function (n=100, polygon=moscow) {
-      const bbox = turf.bbox(polygon);
-      let points = {
-        "ЦАО": { active: true, color: "#ffd700", features: [] },
-        "САО": { active: true, color: "#0000ff", features: [] },
-        "СВАО": { active: true, color: "#4b0082", features: [] },
-        "ВАО": { active: true, color: "#87cefa", features: [] },
-        "ЮВАО": { active: true, color: "#800080", features: [] },
-        "ЮАО": { active: true, color: "#ff0000", features: [] },
-        "ЮЗАО": { active: true, color: "#00ffff", features: [] },
-        "ЗАО": { active: true, color: "#00ff00", features: [] },
-        "СЗАО": { active: true, color: "#cc5500", features: [] },
-        "ТАО": { active: true, color: "#90ee90", features: [] },
-        "НАО": { active: true, color: "#ffff00", features: [] },
-        "ЗелАО": { active: true, color: "#87cefa", features: [] },
-      };
-
-      for (let i = 0; i < n; i++) {
-        let point = turf.randomPoint(1, { bbox: bbox }).features[0];
-
-        if (turf.inside(point, polygon)) {
-          for (let unit of admin_units.features) {
-            if (turf.inside(point, unit)) {
-              points[unit.properties.ref].features.push(point);
-              break;
-            }
-          }
-        } else {
-          i -= 1;
-        }
-      }
-
-      this.points = points;
+    generate: function () {
+      this.points = this.$refs.map.generate();
     },
+
     toggleUnit: function (unit) {
       const entity = Object.entries(this.points).map(([key, value]) => {
         let result = value;
--- src/components/map.vue
+++ src/components/map.vue
@@ -5,6 +5,8 @@
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import * as turf from "@turf/turf";
+import moscow from "../../public/moscow.json";
 import admin_units from "../../public/admin_units.json";
 
 export default {
@@ -60,7 +62,42 @@ export default {
           this.markers.push(group);
         }
       }
-    }
+    },
+    
+    generate: function (n=100, polygon=moscow) {
+      const bbox = turf.bbox(polygon);
+      let points = {
+        "ЦАО": { active: true, color: "#ffd700", features: [] },
+        "САО": { active: true, color: "#0000ff", features: [] },
+        "СВАО": { active: true, color: "#4b0082", features: [] },
+        "ВАО": { active: true, color: "#87cefa", features: [] },
+        "ЮВАО": { active: true, color: "#800080", features: [] },
+        "ЮАО": { active: true, color: "#ff0000", features: [] },
+        "ЮЗАО": { active: true, color: "#00ffff", features: [] },
+        "ЗАО": { active: true, color: "#00ff00", features: [] },
+        "СЗАО": { active: true, color: "#cc5500", features: [] },
+        "ТАО": { active: true, color: "#90ee90", features: [] },
+        "НАО": { active: true, color: "#ffff00", features: [] },
+        "ЗелАО": { active: true, color: "#87cefa", features: [] },
+      };
+
+      for (let i = 0; i < n; i++) {
+        let point = turf.randomPoint(1, { bbox: bbox }).features[0];
+
+        if (turf.inside(point, polygon)) {
+          for (let unit of admin_units.features) {
+            if (turf.inside(point, unit)) {
+              points[unit.properties.ref].features.push(point);
+              break;
+            }
+          }
+        } else {
+          i -= 1;
+        }
+      }
+
+      return points;
+    },
   }
 };
 </script>
